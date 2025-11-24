4040856d59350cce64987df5901910ad5fdddb0c
kaittisak <kaitisak.chaychoopho@e-tech.ac.th>
2025-11-24 09:13:09 +0700

    "Second Commit"

--- lib/main.dart
+++ lib/main.dart
@@ -48,9 +48,10 @@ class MyHomePage extends StatelessWidget {
                   ),
                   SizedBox(height: 10),
                   Container(
-                    padding: EdgeInsets.all(4),
+                    padding: EdgeInsets.all(6),
                     decoration: BoxDecoration(
-                      color: Colors.white,
+                      color: Colors.red,
+
                       shape: BoxShape.circle,
                     ),
                     child: ClipOval(
@@ -69,7 +70,7 @@ class MyHomePage extends StatelessWidget {
                   Text("kaitisak.chaychoopho@e-tecch.ac.th"),
                 ],
               ),
-            ) ,
+            ),
             Container(
               width: double.infinity,
               padding: EdgeInsets.all(20),
@@ -94,7 +95,7 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ],
                   ),
-                  SizedBox(height: 10,),
+                  SizedBox(height: 10),
                   Row(
                     children: [
                       Container(
@@ -113,7 +114,7 @@ class MyHomePage extends StatelessWidget {
                     ],
                   ),
 
-                  SizedBox(height: 10,),
+                  SizedBox(height: 10),
                   Row(
                     children: [
                       Container(
@@ -132,7 +133,7 @@ class MyHomePage extends StatelessWidget {
                     ],
                   ),
 
-                  SizedBox(height: 10,),
+                  SizedBox(height: 10),
                   Row(
                     children: [
                       Container(
@@ -146,19 +147,20 @@ class MyHomePage extends StatelessWidget {
                       SizedBox(width: 10),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [Text("การศึกษา"), Text("เทคโนโลยีภาคตะวันออก(อีเทค)")],
+                        children: [
+                          Text("การศึกษา"),
+                          Text("เทคโนโลยีภาคตะวันออก(อีเทค)"),
+                        ],
                       ),
                     ],
                   ),
                   SizedBox(width: 10),
                   ElevatedButton(
-                    onPressed : () => Navigator.pushNamed(context, '/second'),
+                    onPressed: () => Navigator.pushNamed(context, '/second'),
                     style: ElevatedButton.styleFrom(
-                      fixedSize: Size(2000,40),
+                      fixedSize: Size(2000, 40),
                       backgroundColor: Colors.orange,
                       foregroundColor: Colors.white,
-
-
                     ),
                     child: Text("Go to second"),
                   ),
@@ -177,8 +179,149 @@ class SecondPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        child: ClipOval(
+                          child: Image.network(
+                            "https://upload.wikimedia.org/wikipedia/en/5/5f/Original_Doge_meme.jpg",
+                            width: 80,
+                            height: 80,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 15),
+                      Column(children: [Text("5"), Text("กำลังติดตาม")]),
+                      SizedBox(width: 15),
+                      Container(
+                        width: 1.5,
+                        height: 40,
+                        color: Colors.grey.shade400,
+                      ),
+                      SizedBox(width: 15),
+                      Column(children: [Text("3k"), Text("ผู้ติดตาม")]),
+                      SizedBox(width: 15),
+                      Container(
+                        width: 1.5,
+                        height: 40,
+                        color: Colors.grey.shade400,
+                      ),
+                      SizedBox(width: 15),
+                      Column(children: [Text("300"), Text("ถูกใจและบันทึก")]),
+                    ],
+                  ),
+                  SizedBox(width: 20, height: 20),
+                  Container(
+                    width: double.infinity,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Kaittisak Chaychoophon",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(Icons.verified, color: Colors.blue),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 20, height: 5),
+                  Container(
+                    width: double.infinity,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(Icons.tiktok, color: Colors.grey),
+                        Text(
+                          "Kaittisak",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(Icons.arrow_drop_down, color: Colors.grey),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 10, height: 10),
+                  Container(
+                    padding: EdgeInsets.only(top: 10, left: 0),
+                    width: double.infinity,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(width: 10),
+                        ElevatedButton(
+                          onPressed: () =>
+                              Navigator.pushNamed(context, '/second'),
+                          child: Text(
+                            "ติดตาม",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 15,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: Size(300, 40),
+                            backgroundColor: Colors.yellow,
+                            foregroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              SizedBox(width: 50, height: 13),
+                              Icon(Icons.share, color: Colors.black),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
 
-    return Scaffold(body: Center(child: Text("SecondPage")));
+                  Container(
+                    padding: EdgeInsets.only(top: 0, bottom: 10, left: 12),
+                    width: double.infinity,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
 
+                      children: [
+                        SizedBox(width: 10),
+                        Image.network(
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSEcJnrFtqDth4q7449H9SOHrwRN5FJR7WMg&s",
+                          width: 165,
+                          height: 320,
+                        ),
+                        SizedBox(width: 10),
+                        Image.network(
+                          "https://i.pinimg.com/236x/40/85/1a/40851ac35f45d283b8f6acd3a0bae99c.jpg",
+                          width: 170,
+                          height: 320,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
